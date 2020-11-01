02cde08355db7decb4a1422c054302600d500899
Esteban Herrera <estebanhz9919@gmail.com>
2020-10-31 21:59:27 -0500

    Cree un archivo marvel-provider.js para centralizar las peticiones a la Api, este contiene una función asíncrona que crea el endpoint y hace la consulta, además quite el auto-completado que tiene por defecto los inputs y su estado outline lo declare en none en los estilos css.

---
+++ js/marvel-provider.js
@@ -0,0 +1,28 @@
+const herosContent = document.getElementById('content');
+const publicKey = 'bdaabd1f0b1fc9d1ae88285875f5a3cf';
+
+const hash = '7151488404ea3cebd5fe95b07328db79';
+const urlBase = 'https://gateway.marvel.com/v1/public';
+
+
+
+const getHeroes = async () => {
+  try {
+
+    const marvelUrl = `${urlBase}/characters?ts=1&apikey=${publicKey}&hash=${hash}`;
+
+    const resp = await fetch(marvelUrl);
+
+    if (!resp.ok) {
+      throw {
+        status: resp.status,
+        status: resp.statusText
+      }
+    }
+
+    return await resp.json();
+
+  } catch (error) {
+    console.error(error);
+  }
+}
